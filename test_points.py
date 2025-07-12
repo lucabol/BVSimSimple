e758f1901a8b4e584cebeddcc95f18315c0dd8fc
Luca Bolognese <lucabol@microsoft.com>
2025-07-12 11:39:52 +0200

    Remove test_points.py script as part of codebase cleanup

--- test_points.py
+++
@@ -1,9 +0,0 @@
-# /// script
-# requires-python = ">=3.9"
-# dependencies = []
-# ///
-
-from BVRallyStateMachineDict import test_simulate_match_points
-
-if __name__ == "__main__":
-    test_simulate_match_points()
